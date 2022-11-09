00c3f7d3ddd4d27ab817c85215c5ead8cce36ae3
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-11-09 10:58:26 -0300

    fix module name

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,6 +1,6 @@
 import { Create } from "./pages/create.js"
 import { Home } from "./pages/index.js"
-import { Update } from "./pages/update.js"
+import { Update } from "./pages/Update.js"
 import { ScreenManager } from "./ScreenManager.js"
 
 ScreenManager.setAtualScreen(Home())
