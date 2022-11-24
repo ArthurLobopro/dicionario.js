e21d11131f7901f0bfa8c574df81443611934bb5
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-11-24 19:37:29 -0300

    chore: update config screen

--- src/renderer/pages/Config.js
+++ src/renderer/pages/Config.js
@@ -13,81 +13,90 @@ export function Config() {
             CreateElement("div", {
                 className: "dashed-border spacing-16",
                 content: [
-                    CreateElement('div', {
-                        className: "lines",
+                    CreateElement("div", {
                         content: [
-                            CreateElement("span", {
-                                content: "Modo escuro"
-                            }),
-                            switcher({
-                                onToggle: (checked) => {
-                                    api.toggleDarkMode()
-                                    document.body.classList.toggle("dark")
-                                    frame.updateTheme()
-                                },
-                                checked: api.options().darkMode
-                            }),
-                            CreateElement("span", {
-                                content: "Tema do frame"
-                            }),
-                            CreateElement("select", {
-                                className: "select",
+                            CreateElement('div', {
+                                className: "lines",
                                 content: [
-                                    CreateElement("option", {
-                                        content: "Automatico",
-                                        value: "auto"
+                                    CreateElement("span", {
+                                        content: "Modo escuro"
                                     }),
-                                    CreateElement("option", {
-                                        content: "Claro",
-                                        value: "light"
+                                    switcher({
+                                        onToggle: (checked) => {
+                                            api.toggleDarkMode()
+                                            document.body.classList.toggle("dark")
+                                            frame.updateTheme()
+                                        },
+                                        checked: api.options().darkMode
                                     }),
-                                    CreateElement("option", {
-                                        content: "Escuro",
-                                        value: "dark"
+                                    CreateElement("span", {
+                                        content: "Tema do frame"
+                                    }),
+                                    CreateElement("select", {
+                                        className: "select",
+                                        content: [
+                                            CreateElement("option", {
+                                                content: "Automatico",
+                                                value: "auto"
+                                            }),
+                                            CreateElement("option", {
+                                                content: "Claro",
+                                                value: "light"
+                                            }),
+                                            CreateElement("option", {
+                                                content: "Escuro",
+                                                value: "dark"
+                                            })
+                                        ],
+                                        value: api.options().frameTheme,
+                                        onchange: (event) => {
+                                            const frameTheme = event.currentTarget.value
+                                            api.setFrameTheme(frameTheme)
+                                            frame.updateTheme()
+                                        }
+                                    }),
+                                    CreateElement("span", {
+                                        content: "Estilo da janela"
+                                    }),
+                                    CreateElement("select", {
+                                        className: "select",
+                                        content: [
+                                            CreateElement("option", {
+                                                content: "Windows",
+                                                value: "windows"
+                                            }),
+                                            CreateElement("option", {
+                                                content: "Macos",
+                                                value: "macos"
+                                            }),
+                                        ],
+                                        value: api.options().frameStyle,
+                                        onchange: (event) => {
+                                            const frameStyle = event.currentTarget.value
+                                            api.setFrameStyle(frameStyle)
+                                            frame.setFrameStyle(frameStyle)
+                                        }
                                     })
-                                ],
-                                value: api.options().frameTheme,
-                                onchange: (event) => {
-                                    const frameTheme = event.currentTarget.value
-                                    api.setFrameTheme(frameTheme)
-                                    frame.updateTheme()
-                                }
+                                ]
                             }),
-                            CreateElement("span", {
-                                content: "Estilo da janela"
-                            }),
-                            CreateElement("select", {
-                                className: "select",
+                            CreateElement("div", {
+                                className: "flex-center fill-line",
                                 content: [
-                                    CreateElement("option", {
-                                        content: "Windows",
-                                        value: "windows"
-                                    }),
-                                    CreateElement("option", {
-                                        content: "Macos",
-                                        value: "macos"
-                                    }),
-                                ],
-                                value: api.options().frameStyle,
-                                onchange: (event) => {
-                                    const frameStyle = event.currentTarget.value
-                                    api.setFrameStyle(frameStyle)
-                                    frame.setFrameStyle(frameStyle)
-                                }
-                            })
+                                    CreateElement("button", {
+                                        className: "stroke",
+                                        content: "Mostrar ferramentas de desenvolvedor",
+                                        onclick: () => {
+                                            ipcRenderer.send("open-devtolls")
+                                        }
+                                    })
+                                ]
+                            }),
                         ]
                     }),
-                    CreateElement("div", {
-                        className: "flex-center",
-                        content: [
-                            CreateElement("button", {
-                                className: "stroke",
-                                content: "Mostrar ferramentas de desenvolvedor",
-                                onclick: () => {
-                                    ipcRenderer.send("open-devtolls")
-                                }
-                            })
-                        ]
+
+                    CreateElement("span", {
+                        className: "version",
+                        content: `Versão ${api.version()}`
                     })
                 ]
             })
