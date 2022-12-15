ad4cf763d70376375cb11d7a50697efc48957af9
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-12-15 10:55:19 -0300

    refactor: change Store to ts

---
+++ src/store/Store.ts
@@ -0,0 +1,62 @@
+import ElectronStore from 'electron-store'
+
+export const data = new ElectronStore({
+    name: "data",
+    watch: true,
+    schema: {
+        palavras: {
+            type: "array",
+            default: [],
+            items: {
+                type: "object",
+                required: ["palavra", "definicao", "registro"],
+                properties: {
+                    palavra: {
+                        type: "string"
+                    },
+                    definicao: {
+                        type: "string"
+                    },
+                    registro: {
+                        type: "string",
+                        format: "date-time"
+                    },
+                    ultimaEdicao: {
+                        anyOf: [
+                            {
+                                type: "string",
+                                format: "date-time"
+                            },
+                            {
+                                type: "null"
+                            }
+                        ]
+                    }
+                }
+            }
+        }
+    }
+})
+
+export const options = new ElectronStore({
+    name: "options",
+    watch: true,
+    schema: {
+        darkMode: {
+            type: "boolean",
+            default: true
+        },
+        frameStyle: {
+            type: "string",
+            enum: ["windows", "macos"],
+            default: "windows"
+        },
+        frameTheme: {
+            type: "string",
+            enum: ["light", "dark", "auto"],
+            default: "auto"
+        }
+    }
+})
+
+data.get("palavras") ? null : data.store.palavras = []
