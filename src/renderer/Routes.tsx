b8b47224ffac5301367302d02034de489cf2e069
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-12-29 19:17:16 -0300

    refactor: navigation to config screen

--- src/renderer/Routes.tsx
+++ src/renderer/Routes.tsx
@@ -1,11 +1,13 @@
 import { Route, HashRouter as Router, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
+import { ConfigScreen } from "./pages/Config"
 
 export function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/config" element={<ConfigScreen />} />
             </Routes>
         </Router>
     )
--- src/renderer/pages/Home.tsx
+++ src/renderer/pages/Home.tsx
@@ -3,6 +3,7 @@ import { EyeIcon } from "../components/icons/Eye"
 import { ConfigIcon } from "../components/icons/Config"
 import { Header } from "../components/Header"
 import { Page } from "../components/Page"
+import { useNavigate } from "react-router-dom"
 
 function Option({ text, icon, onclick }: { text: string; icon: JSX.Element; onclick: () => void }) {
     return (
@@ -16,13 +17,15 @@ function Option({ text, icon, onclick }: { text: string; icon: JSX.Element; oncl
 }
 
 export function Home() {
+    const navigate = useNavigate()
+
     return (
         <Page id="home">
             <Header title="Dicionário Pessoal" />
             <div className="option-wrapper">
                 <Option text="Adicionar" icon={<AddIcon />} onclick={() => alert("Adicionar")} />
                 <Option text="Visualizar" icon={<EyeIcon />} onclick={() => alert("Visualizar")} />
-                <Option text="Configurações" icon={<ConfigIcon />} onclick={() => alert("Configurações")} />
+                <Option text="Configurações" icon={<ConfigIcon />} onclick={() => navigate("config")} />
             </div>
         </Page>
     )
