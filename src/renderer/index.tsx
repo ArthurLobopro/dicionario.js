95215a2e9556f9aaa946c16ef25cd6c088d9182d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-12-29 17:14:46 -0300

    refactor: change index to tsx

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,7 @@
 import { ipcRenderer } from "electron"
 import { api } from "../store/Api"
 import { Release } from "./components/modals/Release"
-import { ScreenManager } from "./ScreenManager"
+// import { ScreenManager } from "./ScreenManager"
 
 if (api.options.darkMode) {
     document.body.classList.add("dark")
@@ -17,6 +17,20 @@ ipcRenderer.on("update-downloaded", () => {
     }).append(document.body)
 })
 
-ScreenManager.setAtualScreen("home")
+// ScreenManager.setAtualScreen("home")
+
+import ReactDOM from "react-dom/client"
+import React from "react"
+import { Home } from "./pages/Home"
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement
+)
+
+root.render(
+    <React.StrictMode>
+        <Home />
+    </React.StrictMode>
+)
 
 require("./Frame")
