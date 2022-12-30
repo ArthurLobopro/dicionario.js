f202fda3ef910812a29b9f2b29329919cc49ec88
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-12-29 23:15:22 -0300

    refactor: add routes to screens

--- src/renderer/Routes.tsx
+++ src/renderer/Routes.tsx
@@ -1,13 +1,17 @@
 import { Route, HashRouter as Router, Routes } from "react-router-dom"
 import { Home } from "./pages/Home"
 import { ConfigScreen } from "./pages/Config"
+import { ViewScreen } from "./pages/View"
+import { CreateScreen } from "./pages/Create"
 
 export function AppRoutes() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/create" element={<CreateScreen />} />
                 <Route path="/config" element={<ConfigScreen />} />
+                <Route path="/view" element={<ViewScreen />} />
             </Routes>
         </Router>
     )
