f6f84299ff2722b9e336b2e21a16c760ed643df6
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-12-30 12:35:49 -0300

    refactor: parse release modal to tsx

---
+++ src/renderer/components/modals/Release.tsx
@@ -0,0 +1,24 @@
+import { ModalWrapper } from "./Wrapper"
+
+interface releaseModalProps {
+    onClose: (v: boolean) => void
+}
+
+export function ReleaseModal(props: releaseModalProps) {
+    return (
+        <ModalWrapper>
+            <div className="modal">
+                <div className="modal-header">
+                    Atualização disponível
+                </div>
+                <div className="modal-body">
+                    Uma nova atualização está disponível, reinicie o aplicativo para atualizar.
+                </div>
+                <div className="modal-footer">
+                    <button onClick={() => props.onClose(true)}>Reiniciar Agora</button>
+                    <button className="stroke" onClick={() => props.onClose(false)}>Atualizar Depois</button>
+                </div>
+            </div>
+        </ModalWrapper>
+    )
+}
