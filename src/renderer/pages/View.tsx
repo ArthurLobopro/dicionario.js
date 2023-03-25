e0ced2602f17c8247b73f7323aee4d8c9fbe1a5f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-03-24 22:47:32 -0300

    feat: empty screen added

--- src/renderer/pages/View.tsx
+++ src/renderer/pages/View.tsx
@@ -7,7 +7,7 @@ import { ReturnButton } from "../components/ReturnButton"
 import { ConfirmModal } from "../components/modals/Confirm"
 import { ViewModal } from "../components/modals/View"
 import { useModal } from "../hooks/useModal"
-import { EditIcon, EyeIcon, TrashIcon } from "../components/icons"
+import { EditIcon, EyeIcon, GrayEmptyBookIcon, TrashIcon } from "../components/icons"
 
 export function ViewScreen() {
     const [words, setWords] = useState(Object.entries(api.words))
@@ -32,41 +32,65 @@ export function ViewScreen() {
         />)
     }
 
+    const contents = {
+        get words() {
+            return (
+                <div>
+                    <div className="word-wrapper">
+                        {words.map(([word, word_props]) => (
+                            <div className="word" key={word}>
+                                <div className="content">
+                                    <div className="word-header">
+                                        {word}
+                                    </div>
+                                    <div className="word-definition">
+                                        {word_props.definition}
+                                    </div>
+                                </div>
+                                <div className="controls">
+                                    <div title="Visualizar" onClick={() => ShowViewModal(word)}>
+                                        <EyeIcon />
+                                    </div>
+                                    <div title="Editar" id="edit"
+                                        onClick={() => {
+                                            navigate(`/update/${word}`)
+                                        }}
+                                    >
+                                        <EditIcon />
+                                    </div>
+                                    <div title="Apagar" id="delete" onClick={() => DeleteWord(word)}>
+                                        <TrashIcon />
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )
+        },
+        get empty() {
+            return (
+                <div className="empty">
+
+                    <GrayEmptyBookIcon />
+
+                    <div className="empty-text">
+                        Você ainda não cadastrou nenhuma palavra. Que tal começar agora?
+                    </div>
+
+                    <button onClick={() => navigate("/create")}>
+                        Cadastrar Palavra
+                    </button>
+                </div>
+            )
+        }
+    }
+
     return (
         <Page id="view">
             {modal.content}
             <Header title="Visualizar Palavras" left={<ReturnButton />}></Header>
-            <div>
-                <div className="word-wrapper">
-                    {words.map(([word, word_props]) => (
-                        <div className="word" key={word}>
-                            <div className="content">
-                                <div className="word-header">
-                                    {word}
-                                </div>
-                                <div className="word-definition">
-                                    {word_props.definition}
-                                </div>
-                            </div>
-                            <div className="controls">
-                                <div title="Visualizar" onClick={() => ShowViewModal(word)}>
-                                    <EyeIcon />
-                                </div>
-                                <div title="Editar" id="edit"
-                                    onClick={() => {
-                                        navigate(`/update/${word}`)
-                                    }}
-                                >
-                                    <EditIcon />
-                                </div>
-                                <div title="Apagar" id="delete" onClick={() => DeleteWord(word)}>
-                                    <TrashIcon />
-                                </div>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
+            {words.length > 0 ? contents.words : contents.empty}
         </Page>
     )
 }
