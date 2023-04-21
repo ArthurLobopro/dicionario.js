309ffc93d7dbb7dedac3587b10681262ac01f210
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-04-21 12:00:39 -0300

    feat: add error modal

---
+++ src/renderer/components/modals/Error.tsx
@@ -0,0 +1,23 @@
+import { ErrorIcon } from "../icons"
+import { AlertModal, AlertModalProps } from "./Alert"
+
+const DefaultTitle = () => (
+    <div className="flex gap-4 align-center">
+        Erro
+        <ErrorIcon />
+    </div>
+)
+
+type ErrorModalProps = Omit<AlertModalProps, "title"> & Partial<Pick<AlertModalProps, "title">>
+
+export function SuccessModal(props: ErrorModalProps) {
+    const {
+        title = <DefaultTitle />,
+        message,
+        onClose
+    } = props
+
+    return (
+        <AlertModal title={title} message={message} onClose={onClose} />
+    )
+}
