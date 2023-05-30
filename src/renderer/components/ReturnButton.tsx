5b9c38290e63a6920a8fed9cb794611845f8f8c6
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-05-30 20:12:56 -0300

    refactor: use memo in return button component

--- src/renderer/components/ReturnButton.tsx
+++ src/renderer/components/ReturnButton.tsx
@@ -1,16 +1,20 @@
+import { memo } from "react"
 import { useNavigate } from "react-router-dom"
-import { ReturnIcon } from "./icons"
 import { CircleButton } from "./CircleButton"
+import { ReturnIcon } from "./icons"
 
 interface ReturnButtonProps {
     returnTo?: string
 }
 
-export function ReturnButton({ returnTo = "/" }: ReturnButtonProps) {
-    const navigate = useNavigate()
-    return (
-        <CircleButton title="Voltar" onClick={() => navigate(returnTo)} >
-            <ReturnIcon />
-        </CircleButton>
-    )
-}
+export const ReturnButton = memo(
+    function ReturnButton({ returnTo = "/" }: ReturnButtonProps) {
+        const navigate = useNavigate()
+
+        return (
+            <CircleButton title="Voltar" onClick={() => navigate(returnTo)} >
+                <ReturnIcon />
+            </CircleButton>
+        )
+    }
+)
