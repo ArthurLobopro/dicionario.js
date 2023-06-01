838fbac353692b80c3dc682da9a9fbbf8105a2f5
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-06-01 00:37:49 -0300

    feat: add click validation on return button

--- src/renderer/components/ReturnButton.tsx
+++ src/renderer/components/ReturnButton.tsx
@@ -5,14 +5,24 @@ import { ReturnIcon } from "./icons"
 
 interface ReturnButtonProps {
     returnTo?: string
+    onClick?: () => boolean | Promise<boolean>
 }
 
 export const ReturnButton = memo(
-    function ReturnButton({ returnTo = "/" }: ReturnButtonProps) {
+    function ReturnButton({ returnTo = "/", onClick }: ReturnButtonProps) {
         const navigate = useNavigate()
 
+        async function handleClick() {
+            if (typeof onClick === "function") {
+                const result = await onClick()
+                if (result === false) return
+            }
+
+            navigate(returnTo)
+        }
+
         return (
-            <CircleButton title="Voltar" onClick={() => navigate(returnTo)} >
+            <CircleButton title="Voltar" onClick={handleClick} >
                 <ReturnIcon />
             </CircleButton>
         )
