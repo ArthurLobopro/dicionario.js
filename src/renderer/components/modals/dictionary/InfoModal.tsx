d23973b07aef0633ad1e39ae5f1aaf5ee6bb3204
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-07-03 12:51:15 -0300

    feat: show bigger definition word

--- src/renderer/components/modals/dictionary/InfoModal.tsx
+++ src/renderer/components/modals/dictionary/InfoModal.tsx
@@ -13,6 +13,7 @@ export function DictionaryInfoModal(props: DictionaryInfoModalProps) {
     const wordsCount = props.dictionary.Words.length
     const newerWord = props.dictionary.Words.getNewerWord()
     const olderWord = props.dictionary.Words.getOlderWord()
+    const biggerDefinition = props.dictionary.Words.getBiggerDefinition()
 
     const newerRegister = newerWord?.register && formatDate(newerWord.register.toISOString())
     const olderRegister = olderWord?.register && formatDate(olderWord.register.toISOString())
@@ -38,6 +39,13 @@ export function DictionaryInfoModal(props: DictionaryInfoModalProps) {
                     <p className="flex gap-4 align-center">
                         Último Registro:
                         <span className="info">{olderWord?.word} ({olderRegister})</span>
+                    </p>
+                </If>
+
+                <If condition={!!biggerDefinition}>
+                    <p className="flex gap-4 align-center">
+                        Maior Definição:
+                        <span className="info">{biggerDefinition?.word} ({biggerDefinition?.definition.length} caracteres)</span>
                     </p>
                 </If>
             </>}
