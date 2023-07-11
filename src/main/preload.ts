da8dc4b8281228146ec11e6f5735394ae56b73ae
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-07-10 21:17:46 -0300

    refactor: lint main archives

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,8 +1,9 @@
+import { ipcRenderer } from "electron"
+
 window.addEventListener("DOMContentLoaded", () => {
     require("../renderer/index.js")
 
     if (process.platform === "win32") {
-        const { ipcRenderer } = require("electron")
         ipcRenderer.once("open-in", (event, path) => {
             window.location.hash = path
         })
