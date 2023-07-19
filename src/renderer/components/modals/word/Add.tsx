b559e3759314c04f10c74714b7fb6ab221c78da7
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-07-19 01:30:15 -0300

    feat: Add word modal

---
+++ src/renderer/components/modals/word/Add.tsx
@@ -0,0 +1,171 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useEffect } from "react"
+import { FieldErrors, useForm } from "react-hook-form"
+import { ZodError, z } from "zod"
+import { ErrorModal, SuccessModal, WarningModal } from ".."
+import { api } from "../../../../store/Api"
+import { DictionaryController } from "../../../../store/Controllers/Dictionary"
+import { wordSchema } from "../../../../store/ZodSchemas/word"
+import { frame } from "../../../Frame"
+import { useModal } from "../../../hooks/useModal"
+import { CircleButton, Form, ValidatedInput } from "../../base"
+import { CloseIcon } from "../../icons"
+import { Modal } from "../base/Modal"
+import { ModalWrapper } from "../base/Wrapper"
+
+const create_word_schema = wordSchema.pick({
+  word: true,
+  definition: true,
+})
+
+type CreateWordData = z.infer<typeof create_word_schema>
+
+interface AddWordModalProps {
+  dictionary: DictionaryController
+  onClose: (v?: boolean) => void
+}
+
+export function AddWordModal(props: AddWordModalProps) {
+  const { register, handleSubmit, watch } = useForm<CreateWordData>({
+    resolver: zodResolver(create_word_schema),
+    defaultValues: {
+      word: "",
+      definition: "",
+    },
+  })
+
+  const { dictionary } = props
+
+  const { word, definition } = watch()
+
+  const word_already_exists = (() => {
+    try {
+      return dictionary.Words.getWord(word)?.word === word
+    } catch (error) {
+      return false
+    }
+  })()
+
+  const modal = useModal()
+
+  async function askShouldClose(): Promise<boolean> {
+    return new Promise((resolve) => {
+      modal.open(
+        <WarningModal
+          onClose={(value) => {
+            resolve(value)
+            modal.close()
+          }}
+        >
+          Você tem certeza que deseja sair? Os dados não salvos serão perdidos.
+        </WarningModal>,
+      )
+    })
+  }
+
+  const hasChanges = word.length + definition.length > 0
+
+  const shouldClose = async (): Promise<boolean> => {
+    if (!hasChanges) return true
+
+    return await askShouldClose()
+  }
+
+  function onClose() {
+    if (hasChanges) {
+      askShouldClose().then((should_close) => {
+        should_close && props.onClose()
+      })
+    } else {
+      props.onClose()
+    }
+  }
+
+  useEffect(() => {
+    frame.instance.setBeforeCloseCallback(shouldClose)
+
+    return () => {
+      frame.instance.setBeforeCloseCallback()
+    }
+  }, [word.length + definition.length > 0])
+
+  function onError(errors: FieldErrors) {
+    const message = Object.values(errors)
+      .map((error) => error?.message)
+      .join("\n")
+    modal.open(<ErrorModal message={message} onClose={modal.close} />)
+  }
+
+  function onSubmit(data: CreateWordData) {
+    if (!hasChanges) return props.onClose()
+
+    try {
+      api.dictionaries.getDictionary(dictionary.name).Words.addWord(data)
+
+      modal.open(
+        <SuccessModal
+          message="Palavra adicionada com sucesso!"
+          onClose={props.onClose.bind(null, true)}
+        />,
+      )
+    } catch (error: unknown) {
+      if (error instanceof ZodError) {
+        const zod_error = error as ZodError
+        const message = zod_error.issues
+          .map((issue) => issue.message)
+          .join("\n")
+        modal.open(<ErrorModal onClose={modal.close} message={message} />)
+      } else {
+        const message = (error as Error).message
+        modal.open(<ErrorModal message={message} onClose={modal.close} />)
+      }
+    }
+  }
+
+  return (
+    <ModalWrapper>
+      <Modal type="alert" className="full word-action" onClose={props.onClose}>
+        <div className="dashed-border spacing-16 flex-column fill-heigth">
+          {modal.content}
+
+          <CircleButton title="Fechar" onClick={onClose} useDiv={true}>
+            <CloseIcon />
+          </CircleButton>
+
+          <Form
+            className="grid-fill-center gap fill-heigth"
+            onSubmit={handleSubmit(onSubmit, onError)}
+          >
+            <label className="flex-column gap-4">
+              Palavra
+              <ValidatedInput
+                register={register("word")}
+                hasError={word_already_exists}
+                errorMessage={
+                  word_already_exists
+                    ? `A palavra "${word}" já existe neste dicionário.`
+                    : ""
+                }
+                inputProps={{
+                  placeholder: "Palavra",
+                  autoFocus: true,
+                }}
+              />
+            </label>
+
+            <label className="t-wrapper grid-fill-bottom gap-4">
+              Significado
+              <textarea
+                minLength={5}
+                placeholder="Significados que a palavra pode ter"
+                {...register("definition")}
+              ></textarea>
+            </label>
+
+            <button type="submit">{hasChanges ? "Adicionar" : "Fechar"}</button>
+          </Form>
+        </div>
+      </Modal>
+    </ModalWrapper>
+  )
+}
