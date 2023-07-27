0da17bb0f0ee1250910dcc777f7862453796fda6
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-07-27 09:38:48 -0300

    feat: wrap component on useMemo to improve performance

--- src/renderer/pages/View/components/WordList.tsx
+++ src/renderer/pages/View/components/WordList.tsx
@@ -7,25 +7,30 @@ export function WordList() {
   const { words, search, reload, dictionary, modal } = useContext(ViewContext)
 
   const filter = useMemo(() => new RegExp(`^${search.trim()}`), [search])
+
   const filtered_words = useMemo(() => {
     return words.filter(([word]) => filter.test(word))
-  }, [filter, words])
+  }, [filter, words.length, words])
 
-  return filtered_words.length === 0 ? (
-    <EmptySearch />
-  ) : (
-    <div>
-      <div className="word-wrapper">
-        {filtered_words.map(([word, word_props]) => (
-          <Word
-            word={{ ...word_props, word }}
-            reload={reload}
-            dictionary={dictionary}
-            modal={modal}
-            key={word}
-          />
-        ))}
+  const list = useMemo(() => {
+    return filtered_words.length === 0 ? (
+      <EmptySearch />
+    ) : (
+      <div>
+        <div className="word-wrapper">
+          {filtered_words.map(([word, word_props]) => (
+            <Word
+              word={{ ...word_props, word }}
+              reload={reload}
+              dictionary={dictionary}
+              modal={modal}
+              key={word}
+            />
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }, [filtered_words])
+
+  return list
 }
