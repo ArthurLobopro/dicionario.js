27c1211047d21793bbc78265e52dc4404c40668f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-07-27 11:52:20 -0300

    refactor: move others section to component

--- src/renderer/pages/Config/Config.tsx
+++ src/renderer/pages/Config/Config.tsx
@@ -1,16 +1,11 @@
-import { ipcRenderer, shell } from "electron"
 import { useEffect, useRef, useState } from "react"
 import { api } from "../../../store/Api"
-import { hoverFocus } from "../../Util"
-import { GithubLogo, LinkExternalIcon } from "../../components/icons"
+import { Header, Page, ReturnButton } from "../../components/base"
 import { useModal } from "../../hooks/useModal"
-import { DictionarySection } from "./components/DictionarySection"
-import { WindowSection } from "./components/WindowSection"
-
-import { Header, LineTitle, Page, ReturnButton } from "../../components/base"
 import { AppearanceSection } from "./components/AppearanceSection"
-
-const GITHUB_LINK = "https://github.com/ArthurLobopro/dicionario.js"
+import { DictionarySection } from "./components/DictionarySection"
+import { OthersSection } from "./components/OthersSection"
+import { WindowSection } from "./components/WindowSection"
 
 export function ConfigScreen() {
   const modal = useModal()
@@ -29,11 +24,6 @@ export function ConfigScreen() {
     }
   }, [wrapperRef])
 
-  const openReport = () =>
-    shell.openExternal("https://github.com/ArthurLobopro/dicionario.js/issues")
-  const openGithub = () => shell.openExternal(GITHUB_LINK)
-  const openDevtools = () => ipcRenderer.send("open-devtolls")
-
   return (
     <Page id="config">
       {modal.content}
@@ -51,38 +41,7 @@ export function ConfigScreen() {
 
               <DictionarySection modal={modal} />
 
-              <LineTitle title="Outros" />
-
-              <span>Relatar erro</span>
-              <button
-                className="stroke"
-                title="Relatar erro"
-                onClick={openReport}
-                onMouseEnter={hoverFocus}
-              >
-                <LinkExternalIcon />
-                Abrir
-              </button>
-
-              <span>Sobre</span>
-              <button
-                className="stroke"
-                title="Abrir GitHub"
-                onClick={openGithub}
-                onMouseEnter={hoverFocus}
-              >
-                <GithubLogo />
-                Github
-              </button>
-
-              <button
-                className="stroke fill-center"
-                title="Abrir ferramentas de desenvolvedor"
-                onClick={openDevtools}
-                onMouseEnter={hoverFocus}
-              >
-                Mostrar ferramentas de desenvolvedor
-              </button>
+              <OthersSection />
             </div>
 
             <span className="version">{`Versão ${api.version}`}</span>
