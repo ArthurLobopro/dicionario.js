824ee93d9b917c175a35b5958c5052f6e5516994
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-08-12 09:28:44 -0300

    refactor: re-make delete dictionary modal

--- src/renderer/components/modals/dictionary/DeleteDictionary.tsx
+++ src/renderer/components/modals/dictionary/DeleteDictionary.tsx
@@ -1,38 +1,26 @@
-import { useForm } from "react-hook-form"
+import { FormEvent } from "react"
 import { api } from "../../../../store/Api"
-import { DictionariesController } from "../../../../store/Controllers/Dictionaries"
+import { DictionaryController } from "../../../../store/Controllers/Dictionary"
 import { useModal } from "../../../hooks/useModal"
-import { SelectDictionary } from "../../selects/Dictionary"
 import { ErrorModal } from "../Error"
 import { FormModal } from "../FormModal"
 import { SuccessModal } from "../Success"
 import { WarningModal } from "../Warning"
 
 interface modal_props {
-  onClose: () => void
-}
-
-interface deleteDictionaryProps {
-  dictionary: string
+  onClose: (v?: boolean) => void
+  dictionary: DictionaryController
 }
 
 export function DeleteDictionaryModal(props: modal_props) {
-  const { handleSubmit, setValue } = useForm<deleteDictionaryProps>({
-    defaultValues: {
-      dictionary: api.dictionaries.getDefaultDictionary().name,
-    },
-  })
-
+  const { dictionary } = props
   const modal = useModal()
 
-  async function onDelete(data: deleteDictionaryProps) {
-    const dictionary = data.dictionary
+  async function onDelete(ev: FormEvent) {
+    ev.preventDefault()
+    ev.stopPropagation()
 
-    if (dictionary === "") {
-      return
-    }
-
-    if (dictionary === DictionariesController.getDefaultDictionary().name) {
+    if (dictionary.isDefault) {
       return modal.open(
         <ErrorModal
           onClose={modal.close}
@@ -43,24 +31,24 @@ export function DeleteDictionaryModal(props: modal_props) {
 
     function deleteDictionary() {
       try {
-        api.dictionaries.removeDictionary(dictionary)
+        api.dictionaries.removeDictionary(dictionary.name)
         modal.open(
           <SuccessModal
-            onClose={props.onClose}
-            message={`Dicionário "${dictionary}" deletado com sucesso`}
+            onClose={() => props.onClose(true)}
+            message={`Dicionário "${dictionary.name}" deletado com sucesso`}
           />,
         )
       } catch (error) {
         modal.open(
           <ErrorModal
             onClose={props.onClose}
-            message={`Houve um erro ao deletar o dicionário "${dictionary}"`}
+            message={`Houve um erro ao deletar o dicionário "${dictionary.name}"`}
           />,
         )
       }
     }
 
-    const word_count = api.dictionaries.getDictionary(dictionary).Words.length
+    const word_count = dictionary.Words.length
 
     if (word_count === 0) {
       return deleteDictionary()
@@ -72,11 +60,11 @@ export function DeleteDictionaryModal(props: modal_props) {
             if (confirmed) {
               deleteDictionary()
             } else {
-              modal.close()
+              props.onClose()
             }
           }}
         >
-          O dicionário <strong>"{dictionary}"</strong> possui{" "}
+          O dicionário <strong>"{dictionary.name}"</strong> possui{" "}
           <strong>{word_count}</strong> palavra{word_count > 1 ? "s" : ""},
           deseja apagá-lo mesmo assim? <br />
           Lembre-se: Esta ação não pode ser desfeita.
@@ -87,14 +75,13 @@ export function DeleteDictionaryModal(props: modal_props) {
 
   return (
     <FormModal
+      onClose={props.onClose}
+      onSubmit={onDelete}
       title="Deletar Dicionário"
       submitText="Deletar"
-      onClose={props.onClose}
-      onSubmit={handleSubmit(onDelete)}
     >
       {modal.content}
-      <span>Deletar o dicinário: </span>
-      <SelectDictionary onChange={(value) => setValue("dictionary", value)} />
+      Deletar o dicionário <strong>{dictionary.name}</strong>?
     </FormModal>
   )
 }
